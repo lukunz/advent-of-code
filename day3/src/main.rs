ad883b87ca8fe09d92691da0a123f92e7e99b9c4
Lukas Kunz <mail@lukaskunz.de>
2022-12-03 21:09:07 +0100

    Implements 2022 Day 3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,16 @@
 use std::collections::HashSet;
 use std::fs;
 
-fn main() {
-    let data = fs::read_to_string("input.txt").expect("Can't read input file");
-    let rucksacks: u32 = data.lines()
+fn item_to_priority(item: char) -> u32 {
+    match item {
+        'a'..='z' => item as u32 - 96,
+        'A'..='Z' => item as u32 - 64 + 26,
+        _ => 0
+    }
+}
+
+fn part1(data: &String) {
+    let sum_of_priorities: u32 = data.lines()
         .map(|line| {
             let (left, right): (&str, &str) = line.split_at(line.len() / 2);
             let left: HashSet<char> = HashSet::from_iter(left.chars());
@@ -11,12 +18,38 @@ fn main() {
 
             let intersect = left.intersection(&right);
 
-            intersect.map(|c| match c {
-                'a'..='z' => c.clone() as u32 - 96,
-                'A'..='Z' => c.clone() as u32 - 64 + 26,
-                _ => 0
-            }).sum::<u32>()
+            intersect.map(|c| item_to_priority(c.clone())).sum::<u32>()
         }).sum();
 
-    println!("{rucksacks}");
+    println!("{sum_of_priorities}");
+}
+
+fn part2(data: &String) {
+    let sum_of_priorities: u32 = data.lines()
+        .map(|line| HashSet::from_iter(line.chars()))
+        .collect::<Vec<HashSet<char>>>()
+        .chunks(3)
+        .map(|group| {
+            if group.is_empty() {
+                return 0;
+            }
+            let mut group = Vec::from(group);
+            let mut result_set = group.pop().unwrap();
+            for line in group {
+                result_set = result_set.intersection(&line)
+                    .map(|c| c.clone())
+                    .collect();
+            }
+
+            result_set.iter().map(|c| item_to_priority(*c)).sum()
+        }).sum();
+
+    println!("{sum_of_priorities}");
+}
+
+fn main() {
+    let data = fs::read_to_string("input.txt").expect("Can't read input file");
+
+    part1(&data);
+    part2(&data);
 }
