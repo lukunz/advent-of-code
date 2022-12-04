9af9ff3ab9a709c700a9a5aa1e05de9bc1fd08f7
Lukas Kunz <mail@lukaskunz.de>
2022-12-04 13:05:23 +0100

    Implements 2022 Day 4 Part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,8 +12,8 @@ fn parse_item(item: &str) -> Option<Pair> {
 
     Some((
         parts[0].parse().ok()?,
-        parts[1].parse().ok()?)
-    )
+        parts[1].parse().ok()?
+    ))
 }
 
 fn parse_line(line: &str) -> Option<Group> {
@@ -30,19 +30,29 @@ fn parse_line(line: &str) -> Option<Group> {
 
 fn check_contains(group: &Group) -> bool {
     group.0.0 >= group.1.0 && group.0.1 <= group.1.1
-    || group.0.0 <= group.1.0 && group.0.1 >= group.1.1
+        || group.0.0 <= group.1.0 && group.0.1 >= group.1.1
+}
+
+fn check_overlap(group: &Group) -> bool {
+    group.0.1 >= group.1.0 && group.0.0 <= group.1.1
+        || group.0.1 <= group.1.0 && group.0.0 >= group.1.1
 }
 
 fn main() {
     let data = fs::read_to_string("input.txt").expect("Can't read input file");
 
-    let number_of_fully_contained_groups = data.lines()
+    let groups: Vec<Group> = data.lines()
         .filter_map(|line| parse_line(line))
+        .collect();
+
+    let number_of_fully_contained_groups = groups.iter()
         .filter(|&group| check_contains(&group))
-        .collect::<Vec<Group>>()
-        .len();
+        .count();
 
-
+    let number_of_overlapping_groups = groups.iter()
+        .filter(|&group| check_overlap(&group))
+        .count();
 
     println!("{number_of_fully_contained_groups}");
+    println!("{number_of_overlapping_groups}");
 }
