5083543ab98eabd757e9b541f685f0c93c0f6b7c
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2022-12-10 00:05:04 +0100

    Implements 2022 Day 9 Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -17,8 +17,10 @@ enum Action {
 fn main() {
     let actions = read_input_file("day9/input.txt");
     let part_one = part_one(&actions);
+    let part_two = part_two(&actions);
 
-    println!("Part ont: {part_one}");
+    println!("Part one: {part_one}");
+    println!("Part two: {part_two}");
 }
 
 fn part_one(actions: &Vec<Action>) -> usize {
@@ -64,9 +66,56 @@ fn part_one(actions: &Vec<Action>) -> usize {
     visited_points.len()
 }
 
+fn part_two(actions: &Vec<Action>) -> usize {
+    let mut rope: Vec<Point> = (0..10).map(|_| Point { x: 0, y: 0}).collect();
+    let mut visited_points = HashSet::new();
+
+    for action in actions {
+        match action {
+            Action::Right(distance) => {
+                for _ in 0..*distance {
+                    rope[0].x += 1;
+                    for i in 0..9 {
+                        rope[i + 1] = move_tail(&rope[i], &rope[i + 1]);
+                    }
+                    visited_points.insert(rope[9].clone());
+                }
+            }
+            Action::Left(distance) => {
+                for _ in 0..*distance {
+                    rope[0].x -= 1;
+                    for i in 0..9 {
+                        rope[i + 1] = move_tail(&rope[i], &rope[i + 1]);
+                    }
+                    visited_points.insert(rope[9].clone());
+                }
+            }
+            Action::Down(distance) => {
+                for _ in 0..*distance {
+                    rope[0].y += 1;
+                    for i in 0..9 {
+                        rope[i + 1] = move_tail(&rope[i], &rope[i + 1]);
+                    }
+                    visited_points.insert(rope[9].clone());
+                }
+            }
+            Action::Up(distance) => {
+                for _ in 0..*distance {
+                    rope[0].y -= 1;
+                    for i in 0..9 {
+                        rope[i + 1] = move_tail(&rope[i], &rope[i + 1]);
+                    }
+                    visited_points.insert(rope[9].clone());
+                }
+            }
+        }
+    }
+
+    visited_points.len()
+}
+
 fn move_tail(head: &Point, tail: &Point) -> Point {
     let mut new_tail = Point { x: tail.x, y: tail.y };
-
     let x_distance = head.x - tail.x;
     let y_distance = head.y - tail.y;
 
