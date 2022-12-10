c5d07173b6e4fa3a74f7ee55eb2af13e3f5c2dc0
Lukas Kunz <mail@lukaskunz.de>
2022-12-10 19:47:21 +0100

    Implements 2022 Day 10 Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 enum Operation {
     Noop,
-    Addx(i32),
+    AddX(i32),
 }
 
 struct Cpu {
@@ -12,39 +12,48 @@ struct Cpu {
 }
 
 impl Cpu {
-    fn cycle(&mut self) {
-        self.cycle += 1;
-
+    fn execute(&mut self, operation: &Operation) {
         if (self.cycle + 20) % 40 == 0 {
             self.signal_strength += self.cycle * self.register;
         }
-    }
 
-    fn execute(&mut self, operation: &Operation) {
-        match operation {
-            Operation::Noop => {
-                self.cycle();
-            },
-            Operation::Addx(amount) => {
-                self.cycle();
-                self.register += amount;
-                self.cycle();
-            },
+        if (self.register..self.register + 3).contains(&((self.cycle - 1) % 40 + 1)) {
+            print!("#")
+        } else {
+            print!(".");
         }
+
+        if self.cycle % 40 == 0 {
+            println!();
+        }
+
+        match operation {
+            Operation::Noop => {}
+            Operation::AddX(amount) => {
+                self.register += amount;
+            }
+        }
+
+        self.cycle += 1;
     }
 }
 
 fn main() {
     let data = fs::read_to_string("day10/input.txt").expect("Can't read input file");
-
-    let operations: Vec<Operation> = data.lines().map(|line| {
+    let mut operations: Vec<Operation> = Vec::new();
+    for line in data.lines() {
         let parts = line.split_once(' ');
 
         match parts {
-            None => Operation::Noop,
-            Some((_, amount)) => Operation::Addx(amount.parse().expect("Error parsing input")),
+            None => {
+                operations.push(Operation::Noop);
+            }
+            Some((_, amount)) => {
+                operations.push(Operation::Noop);
+                operations.push(Operation::AddX(amount.parse().expect("Error parsing input")));
+            },
         }
-    }).collect();
+    }
 
     let mut cpu = Cpu {
         register: 1,
