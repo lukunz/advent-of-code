6232e0442618cb4286b0f4e74998b806d209dcbc
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2022-12-12 23:40:48 +0100

    Implemented Day 12 in a simpler an more efficient way

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,24 +1,7 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeMap, VecDeque};
 use std::fs;
 
-#[derive(Debug, Clone)]
-struct Point {
-    x: i64,
-    y: i64,
-}
-
-impl Point {
-    fn as_tuple(&self) -> (i64, i64) {
-        (self.x, self.y)
-    }
-
-    fn from_tuple(tuple: &(i64, i64)) -> Self {
-        Self {
-            x: tuple.0,
-            y: tuple.1,
-        }
-    }
-}
+type Point = (i64, i64);
 
 struct Map {
     points: Vec<Vec<i64>>,
@@ -44,11 +27,11 @@ impl Map {
             for height_char in line.chars() {
                 match height_char {
                     'E' => {
-                        end = Some(Point { x, y });
+                        end = Some((x, y));
                         row.push(25);
                     }
                     'S' => {
-                        start = Some(Point { x, y });
+                        start = Some((x, y));
                         row.push(0);
                     }
                     'a'..='z' => row.push(height_char.to_digit(36).unwrap() as i64 - 10),
@@ -70,35 +53,23 @@ impl Map {
     }
 
     fn contains(&self, point: &Point) -> bool {
-        if point.x < 0 || point.y < 0 {
+        if point.0 < 0 || point.1 < 0 {
             return false;
         }
 
-        (0..self.width).contains(&point.x) && (0..self.height).contains(&point.y)
+        (0..self.width).contains(&point.0) && (0..self.height).contains(&point.1)
     }
 
     fn height(&self, point: &Point) -> i64 {
-        self.points[point.y as usize][point.x as usize]
+        self.points[point.1 as usize][point.0 as usize]
     }
 
     fn neighbours(&self, point: &Point) -> Vec<Point> {
         let edges = vec![
-            Point {
-                x: point.x + 1,
-                y: point.y,
-            },
-            Point {
-                x: point.x,
-                y: point.y + 1,
-            },
-            Point {
-                x: point.x - 1,
-                y: point.y,
-            },
-            Point {
-                x: point.x,
-                y: point.y - 1,
-            },
+            (point.0 + 1, point.1),
+            (point.0, point.1 + 1),
+            (point.0 - 1, point.1),
+            (point.0, point.1 - 1),
         ];
 
         edges
@@ -109,55 +80,30 @@ impl Map {
 
     fn shortest_path(&self, start: &Point) -> i64 {
         let mut distances = BTreeMap::new();
-        let mut queue = BTreeSet::new();
+        let mut queue = VecDeque::new();
 
         for x in 0..self.width {
             for y in 0..self.height {
-                let p = (x, y);
-                queue.insert(p);
-                distances.insert(p, i64::MAX);
+                distances.insert((x, y), i64::MAX);
             }
         }
 
-        distances.insert(start.as_tuple(), 0);
+        distances.insert(*start, 0);
+        queue.push_back(*start);
 
         while !queue.is_empty() {
-            let (point, distance) = distances
-                .iter()
-                .filter(|(p, _)| queue.contains(p))
-                .reduce(|(nearest_point, nearest_distance), (point, distance)| {
-                    if nearest_distance < distance {
-                        (nearest_point, nearest_distance)
-                    } else {
-                        (point, distance)
-                    }
-                })
-                .expect("not possible");
-            queue.remove(point);
+            let point = queue.pop_front().unwrap();
+            let distance = distances.get(&point).unwrap().clone();
 
-            if point == &self.end.as_tuple() {
-                return *distance;
-            }
-
-            if *distance == i64::MAX {
-                continue;
-            }
-
-            let point = point.clone();
-            let distance = distance.clone();
-
-            for neighbour in self.neighbours(&Point::from_tuple(&point)) {
-                if queue.contains(&neighbour.as_tuple()) {
-                    let new_distance = distance + 1;
-
-                    if new_distance < *distances.get(&neighbour.as_tuple()).expect("impossible") {
-                        distances.insert(neighbour.as_tuple(), new_distance);
-                    }
+            for neighbour in self.neighbours(&point) {
+                if distances.get(&neighbour).unwrap() > &(distance + 1) {
+                    distances.insert(neighbour, distance + 1);
+                    queue.push_back(neighbour);
                 }
             }
         }
 
-        *distances.get(&self.end.as_tuple()).expect("impossible")
+        *distances.get(&self.end).unwrap()
     }
 }
 
@@ -169,7 +115,7 @@ fn main() {
     let mut points = Vec::new();
     for x in 0..map.width {
         for y in 0..map.height {
-            points.push(Point { x, y });
+            points.push((x, y));
         }
     }
 
@@ -195,30 +141,30 @@ mod tests {
     fn test_contains() {
         let map = Map::from_file("input-small.txt");
 
-        assert!(map.contains(&Point { x: 0, y: 0 }));
-        assert!(map.contains(&Point { x: 1, y: 0 }));
-        assert!(map.contains(&Point { x: 0, y: 1 }));
-        assert!(map.contains(&Point { x: 1, y: 1 }));
+        assert!(map.contains(&(0, 0)));
+        assert!(map.contains(&(0, 1)));
+        assert!(map.contains(&(1, 0)));
+        assert!(map.contains(&(1, 1)));
 
-        assert!(!map.contains(&Point { x: -1, y: 1 }));
-        assert!(!map.contains(&Point { x: 0, y: 100 }));
+        assert!(!map.contains(&(-1, 1)));
+        assert!(!map.contains(&(0, 100)));
     }
 
     #[test]
     fn test_height() {
         let map = Map::from_file("input-small.txt");
 
-        assert_eq!(0, map.height(&Point { x: 0, y: 0 }));
-        assert_eq!(0, map.height(&Point { x: 1, y: 0 }));
-        assert_eq!(0, map.height(&Point { x: 0, y: 1 }));
-        assert_eq!(1, map.height(&Point { x: 1, y: 1 }));
+        assert_eq!(0, map.height(&(0, 0)));
+        assert_eq!(0, map.height(&(1, 0)));
+        assert_eq!(0, map.height(&(0, 1)));
+        assert_eq!(1, map.height(&(1, 1)));
     }
 
     #[test]
     fn test_neighbours() {
         let map = Map::from_file("input-small.txt");
 
-        let neighbours = map.neighbours(&Point { x: 0, y: 0 });
+        let neighbours = map.neighbours(&(0, 0));
 
         assert_eq!(2, neighbours.len());
     }
