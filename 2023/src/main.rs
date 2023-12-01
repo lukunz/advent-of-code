05ad15640ba9d5bb03f5ac569c891be11c20b5d9
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-01 09:17:47 +0100

    Implements 2023 Day 1 Part 1

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string("day1.txt").expect("Can't read input file");
+
+    let result: i32 = data.lines()
+        .map(|line| line.chars())
+        .map(|mut chars| (
+                chars.clone().find(|c| c.is_numeric()).expect("No digets").to_string().parse::<i32>().unwrap(),
+                chars.rfind(|c| c.is_numeric()).expect("No digets").to_string().parse::<i32>().unwrap()
+            )
+        ).map(|(first_digit, last_digit)| first_digit * 10 + last_digit)
+        .sum();
+
+    println!("Day 1 Part 1: {}", result);
 }
