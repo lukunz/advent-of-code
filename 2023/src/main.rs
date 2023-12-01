102c45d712f1e03a4788c1246ebec0cca76ce8aa
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-01 12:04:15 +0100

    Clenup workspace

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -3,13 +3,27 @@ use std::fs;
 fn main() {
     let data = fs::read_to_string("day1.txt").expect("Can't read input file");
 
-    let result: i32 = data.lines()
+    let result: i32 = data
+        .lines()
         .map(|line| line.chars())
-        .map(|mut chars| (
-                chars.clone().find(|c| c.is_numeric()).expect("No digets").to_string().parse::<i32>().unwrap(),
-                chars.rfind(|c| c.is_numeric()).expect("No digets").to_string().parse::<i32>().unwrap()
+        .map(|mut chars| {
+            (
+                chars
+                    .clone()
+                    .find(|c| c.is_numeric())
+                    .expect("No digits")
+                    .to_string()
+                    .parse::<i32>()
+                    .unwrap(),
+                chars
+                    .rfind(|c| c.is_numeric())
+                    .expect("No digits")
+                    .to_string()
+                    .parse::<i32>()
+                    .unwrap(),
             )
-        ).map(|(first_digit, last_digit)| first_digit * 10 + last_digit)
+        })
+        .map(|(first_digit, last_digit)| first_digit * 10 + last_digit)
         .sum();
 
     println!("Day 1 Part 1: {}", result);
