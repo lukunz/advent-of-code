37f0e7b9971937498e6e820736631c3883c1e1fd
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-03 12:24:45 +0100

    Implements 2023 Day 1 Part 2

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,30 +1,50 @@
+use regex::Regex;
 use std::fs;
+
+fn convert_to_int(value: &str) -> i32 {
+    match value {
+        "1" | "one" => 1,
+        "2" | "two" => 2,
+        "3" | "three" => 3,
+        "4" | "four" => 4,
+        "5" | "five" => 5,
+        "6" | "six" => 6,
+        "7" | "seven" => 7,
+        "8" | "eight" => 8,
+        "9" | "nine" => 9,
+        _ => panic!("Not a digit"),
+    }
+}
+
+fn parse(data: &str, first_re: Regex, last_re: Regex) -> i32 {
+    let mut sum = 0;
+
+    for line in data.lines() {
+        let mut first_matches = first_re.captures_iter(line);
+        let mut last_matches = last_re.captures_iter(line);
+        let (_, [first]) = first_matches.next().expect("No digits found").extract();
+        let (_, [last]) = last_matches.next().expect("No digits found").extract();
+
+        sum += convert_to_int(first) * 10 + convert_to_int(last);
+    }
+
+    sum
+}
 
 fn main() {
     let data = fs::read_to_string("day1.txt").expect("Can't read input file");
 
-    let result: i32 = data
-        .lines()
-        .map(|line| line.chars())
-        .map(|mut chars| {
-            (
-                chars
-                    .clone()
-                    .find(|c| c.is_numeric())
-                    .expect("No digits")
-                    .to_string()
-                    .parse::<i32>()
-                    .unwrap(),
-                chars
-                    .rfind(|c| c.is_numeric())
-                    .expect("No digits")
-                    .to_string()
-                    .parse::<i32>()
-                    .unwrap(),
-            )
-        })
-        .map(|(first_digit, last_digit)| first_digit * 10 + last_digit)
-        .sum();
+    let part1 = parse(
+        &data,
+        Regex::new(r".*?(\d).*").unwrap(),
+        Regex::new(r".*(\d).*?").unwrap(),
+    );
+    let part2 = parse(
+        &data,
+        Regex::new(r".*?(\d|one|two|three|four|five|six|seven|eight|nine).*").unwrap(),
+        Regex::new(r".*(\d|one|two|three|four|five|six|seven|eight|nine).*?").unwrap(),
+    );
 
-    println!("Day 1 Part 1: {}", result);
+    println!("Day 1 Part 1: {}", part1);
+    println!("Day 1 Part 2: {}", part2);
 }
