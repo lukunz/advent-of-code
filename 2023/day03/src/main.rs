d16e59c858bdca452d4acff8670fe3f31163c84a
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-03 16:09:16 +0100

    Implements 2023 Day 3 Part 2

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct NumberLocation {
     line_number: usize,
     start: usize,
@@ -61,6 +61,43 @@ fn is_symbol(ch: char) -> bool {
     !ch.is_numeric() && ch != '.'
 }
 
+fn find_gears(map: &[Vec<char>], col_count: usize) -> Vec<(usize, usize)> {
+    let mut locations: Vec<(usize, usize)> = Vec::new();
+
+    for (y, _) in map.iter().enumerate() {
+        for x in 0..col_count {
+            if map[y][x] == '*' {
+                locations.push((x, y));
+            }
+        }
+    }
+
+    locations
+}
+
+fn bound_inc(a: usize, bound: usize) -> usize {
+    if a < bound {
+        a + 1
+    } else {
+        a
+    }
+}
+
+fn bound_dec(a: usize, bound: usize) -> usize {
+    if bound < a {
+        a - 1
+    } else {
+        a
+    }
+}
+
+fn get_number(map: &[Vec<char>], location: &NumberLocation) -> usize {
+    let number_string =
+        String::from_iter(map[location.line_number][location.start..=location.end].iter());
+
+    number_string.parse::<usize>().unwrap()
+}
+
 fn main() {
     let data = fs::read_to_string("day3.txt").expect("Can't read input file");
 
@@ -71,12 +108,12 @@ fn main() {
     let number_locations = find_number_locations(data);
     let mut valid_number_locations: Vec<NumberLocation> = Vec::new();
 
-    for location in number_locations {
+    for location in number_locations.iter() {
         if location.line_number > 0 {
             // check top row
             for x in location.start..=location.end {
                 if is_symbol(map[location.line_number - 1][x]) {
-                    valid_number_locations.push(location.clone());
+                    valid_number_locations.push(*location);
                 }
             }
         }
@@ -85,7 +122,7 @@ fn main() {
             // check bottom row
             for x in location.start..=location.end {
                 if is_symbol(map[location.line_number + 1][x]) {
-                    valid_number_locations.push(location.clone());
+                    valid_number_locations.push(*location);
                 }
             }
         }
@@ -93,42 +130,42 @@ fn main() {
         if location.start > 0 {
             // check left symbol
             if is_symbol(map[location.line_number][location.start - 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
 
         if location.end < col_count - 1 {
             // check right symbol
             if is_symbol(map[location.line_number][location.end + 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
 
         if location.line_number > 0 && location.start > 0 {
             // check top left symbol
             if is_symbol(map[location.line_number - 1][location.start - 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
 
         if location.line_number > 0 && location.end < col_count - 1 {
             // check top right symbol
             if is_symbol(map[location.line_number - 1][location.end + 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
 
         if location.line_number < row_count - 1 && location.start > 0 {
             // check bottom left
             if is_symbol(map[location.line_number + 1][location.start - 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
 
         if location.line_number < row_count - 1 && location.end < col_count - 1 {
             // check bottom right
             if is_symbol(map[location.line_number + 1][location.end + 1]) {
-                valid_number_locations.push(location.clone());
+                valid_number_locations.push(*location);
             }
         }
     }
@@ -136,12 +173,40 @@ fn main() {
     let mut sum: usize = 0;
 
     for location in valid_number_locations {
-        let number_string =
-            String::from_iter(map[location.line_number][location.start..=location.end].iter());
-
-        // println!("{}", number_string);
-        sum += number_string.parse::<usize>().unwrap();
+        sum += get_number(&map, &location);
     }
 
     println!("Day 3 Part 1 {}", sum);
+
+    let gear_locations = find_gears(&map, col_count);
+    let mut gear_numbers: Vec<Vec<NumberLocation>> = Vec::new();
+
+    for location in gear_locations {
+        let numbers: Vec<NumberLocation> = number_locations
+            .iter()
+            .filter(|num_loc| {
+                (bound_dec(num_loc.line_number, 0)..=bound_inc(num_loc.line_number, row_count))
+                    .contains(&location.1)
+                    && (bound_dec(num_loc.start, 0)..=bound_inc(num_loc.end, row_count))
+                        .contains(&location.0)
+            })
+            .copied()
+            .collect();
+
+        if numbers.len() == 2 {
+            gear_numbers.push(numbers);
+        }
+    }
+
+    let part2_solution: usize = gear_numbers
+        .iter()
+        .map(|numbers| {
+            numbers
+                .iter()
+                .map(|number| get_number(&map, number))
+                .product::<usize>()
+        })
+        .sum();
+
+    println!("Day 3 Part 2: {}", part2_solution);
 }
