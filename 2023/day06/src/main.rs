a311a6ddc2e93d7deac13829647b8d5eb24bd4c9
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-07 21:48:37 +0100

    Implements 2023 Day 6 Part 2

--- 2023/day06/src/main.rs
+++ 2023/day06/src/main.rs
@@ -7,24 +7,43 @@ fn parse_number_list(number_str: &str) -> Vec<usize> {
         .collect()
 }
 
-fn main() {
-    let data = fs::read_to_string("day6.txt").expect("Can't read input file");
-    let mut lines = data.lines();
-    let (_, times) = lines.next().unwrap().split_once(':').unwrap();
-    let (_, records) = lines.next().unwrap().split_once(':').unwrap();
+fn parse_number_ignoring_whitespace(number_str: &str) -> usize {
+    let digits = number_str.chars().filter(|n| n.is_numeric());
 
-    let times = parse_number_list(times);
-    let records = parse_number_list(records);
-    let races = times.iter().zip(records);
+    String::from_iter(digits).parse::<usize>().unwrap()
+}
 
-    let part1_result: usize = races
+fn run_races(races: &[(usize, usize)]) -> usize {
+    races
+        .into_iter()
         .map(|(time, record)| {
             (0..=*time)
                 .map(|t| t * (time - t))
-                .filter(|d| *d > record)
+                .filter(|d| *d > *record)
                 .count()
         })
-        .product();
+        .product()
+}
+
+fn main() {
+    let data = fs::read_to_string("day6.txt").expect("Can't read input file");
+    let mut lines = data.lines();
+    let (_, times_str) = lines.next().unwrap().split_once(':').unwrap();
+    let (_, records_str) = lines.next().unwrap().split_once(':').unwrap();
+
+    let times = parse_number_list(times_str);
+    let records = parse_number_list(records_str);
+    let races: Vec<(usize, usize)> = times.into_iter().zip(records).collect();
+
+    let part1_result: usize = run_races(&races);
+
+    let long_race = (
+        parse_number_ignoring_whitespace(times_str),
+        parse_number_ignoring_whitespace(records_str),
+    );
+
+    let part2_result: usize = run_races(&[long_race]);
 
     println!("Day 6 Part 1: {}", part1_result);
+    println!("Day 6 Part 2: {}", part2_result);
 }
