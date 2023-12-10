ca8851dae7036f01d481c5ba13ce2a86b681d742
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-10 19:52:00 +0100

    Reworked 2023 Day 7 Part 2

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -1,7 +1,6 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs;
-use std::str::FromStr;
 
 #[derive(Debug, PartialOrd, PartialEq, Ord, Eq)]
 enum Strength {
@@ -19,6 +18,7 @@ enum Card {
     A,
     K,
     Q,
+    J,
     T,
     C9,
     C8,
@@ -28,16 +28,22 @@ enum Card {
     C4,
     C3,
     C2,
-    J,
+    Joker,
 }
 
 impl Card {
-    fn from_char(c: char) -> Self {
+    fn from_char(c: char, with_joker: bool) -> Self {
         match c {
             'A' => Self::A,
             'K' => Self::K,
             'Q' => Self::Q,
-            'J' => Self::J,
+            'J' => {
+                if with_joker {
+                    Self::Joker
+                } else {
+                    Self::J
+                }
+            }
             'T' => Self::T,
             '9' => Self::C9,
             '8' => Self::C8,
@@ -59,22 +65,23 @@ struct Hand {
     strength: Strength,
 }
 
-impl FromStr for Hand {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn from_str(s: &str, with_joker: bool) -> Self {
         let (cards, bid) = s.split_once(' ').unwrap();
-        let cards: Vec<Card> = cards.chars().map(Card::from_char).collect();
+        let cards: Vec<Card> = cards
+            .chars()
+            .map(|c| Card::from_char(c, with_joker))
+            .collect();
         let bid = bid.parse::<usize>().unwrap();
 
-        let (card_counts, joker_count) = count_cards(&cards);
-        let strength = calculate_strength(&card_counts, joker_count);
+        let card_counts = count_cards(&cards, with_joker);
+        let strength = calculate_strength(&card_counts);
 
-        Ok(Hand {
+        Hand {
             cards,
             bid,
             strength,
-        })
+        }
     }
 }
 
@@ -110,7 +117,7 @@ impl Ord for Hand {
     }
 }
 
-fn count_cards(cards: &[Card]) -> (Vec<usize>, usize) {
+fn count_cards(cards: &[Card], with_jokers: bool) -> Vec<usize> {
     let mut card_counts: HashMap<Card, usize> = HashMap::new();
 
     for card in cards {
@@ -121,96 +128,92 @@ fn count_cards(cards: &[Card]) -> (Vec<usize>, usize) {
         }
     }
 
-    let joker_count = card_counts.remove(&Card::J).unwrap_or(0);
+    if with_jokers {
+        if let Some(joker_count) = card_counts.remove(&Card::Joker) {
+            if let Some((high_card, count)) =
+                card_counts
+                    .iter()
+                    .reduce(|(card_a, count_a), (card_b, count_b)| {
+                        if count_a > count_b {
+                            (card_a, count_a)
+                        } else {
+                            (card_b, count_b)
+                        }
+                    })
+            {
+                card_counts.insert(*high_card, count + joker_count);
+            } else {
+                card_counts.insert(Card::Joker, joker_count);
+            }
+        }
+    }
 
-    (card_counts.into_values().collect(), joker_count)
+    card_counts.into_values().collect()
 }
 
-fn calculate_strength(card_counts: &[usize], joker_count: usize) -> Strength {
-    if is_five_of_a_kind(card_counts, joker_count) {
+fn calculate_strength(card_counts: &[usize]) -> Strength {
+    if is_five_of_a_kind(card_counts) {
         Strength::FiveOfAKind
-    } else if is_four_of_a_kind(card_counts, joker_count) {
+    } else if is_four_of_a_kind(card_counts) {
         Strength::FourOfAKind
-    } else if is_full_house(card_counts, joker_count) {
+    } else if is_full_house(card_counts) {
         Strength::FullHouse
-    } else if is_three_of_a_kind(card_counts, joker_count) {
+    } else if is_three_of_a_kind(card_counts) {
         Strength::ThreeOfAKind
-    } else if is_two_pair(card_counts, joker_count) {
+    } else if is_two_pair(card_counts) {
         Strength::TwoPair
-    } else if is_one_pair(card_counts, joker_count) {
+    } else if is_one_pair(card_counts) {
         Strength::OnePair
     } else {
         Strength::HighCard
     }
 }
 
-fn is_five_of_a_kind(card_counts: &[usize], joker_count: usize) -> bool {
-    if card_counts.is_empty() {
-        true
-    } else {
-        card_counts.iter().max().unwrap() + joker_count == 5
-    }
+fn is_five_of_a_kind(card_counts: &[usize]) -> bool {
+    card_counts.contains(&5)
 }
 
-fn is_four_of_a_kind(card_counts: &[usize], joker_count: usize) -> bool {
-    if card_counts.is_empty() {
-        false
-    } else {
-        card_counts.iter().max().unwrap() + joker_count == 4
-    }
+fn is_four_of_a_kind(card_counts: &[usize]) -> bool {
+    card_counts.contains(&4)
 }
 
-fn is_full_house(card_counts: &[usize], joker_count: usize) -> bool {
-    if joker_count > 3 {
-        false
-    } else if joker_count == 3 {
-        true
-    } else {
-        card_counts.contains(&3) && card_counts.contains(&2)
-            || joker_count == 1 && card_counts.iter().filter(|c| **c == 2).count() == 2
-            || joker_count == 1 && card_counts.contains(&3)
-            || joker_count == 2 && card_counts.contains(&3)
-            || joker_count == 2 && card_counts.contains(&2)
-    }
+fn is_full_house(card_counts: &[usize]) -> bool {
+    card_counts.contains(&3) && card_counts.contains(&2)
 }
 
-fn is_three_of_a_kind(card_counts: &[usize], joker_count: usize) -> bool {
-    if joker_count > 2 {
-        false
-    } else if joker_count == 2 {
-        true
-    } else {
-        card_counts.contains(&3) && !card_counts.contains(&2)
-            || joker_count == 1 && card_counts.contains(&2)
-    }
+fn is_three_of_a_kind(card_counts: &[usize]) -> bool {
+    card_counts.contains(&3) && !card_counts.contains(&2)
 }
 
-fn is_two_pair(card_counts: &[usize], joker_count: usize) -> bool {
-    if joker_count > 1 {
-        false
-    } else {
-        card_counts.iter().filter(|c| **c == 2).count() == 2
-            || joker_count == 1 && card_counts.contains(&2)
-    }
+fn is_two_pair(card_counts: &[usize]) -> bool {
+    card_counts.iter().filter(|c| **c == 2).count() == 2
 }
 
-fn is_one_pair(card_counts: &[usize], joker_count: usize) -> bool {
-    joker_count == 1 || card_counts.iter().filter(|c| **c == 2).count() == 1
+fn is_one_pair(card_counts: &[usize]) -> bool {
+    card_counts.iter().filter(|c| **c == 2).count() == 1
 }
 
-fn main() {
-    let data = fs::read_to_string("day7.txt").expect("Can't read input file");
-
-    let mut hands: Vec<Hand> = data.lines().map(|h| Hand::from_str(h).unwrap()).collect();
+fn calculate_result(data: String, with_joker: bool) -> usize {
+    let mut hands: Vec<Hand> = data
+        .lines()
+        .map(|h| Hand::from_str(h, with_joker))
+        .collect();
 
     hands.sort();
 
-    let part1_result: usize = hands
+    hands
         .iter()
         .rev()
         .enumerate()
         .map(|(rank, hand)| (rank + 1) * hand.bid)
-        .sum();
+        .sum()
+}
+fn main() {
+    let data = fs::read_to_string("day7.txt").expect("Can't read input file");
 
-    println!("Day 7 Part 2: {}", part1_result);
+    let part1_result = calculate_result(data.clone(), false);
+    let part2_result = calculate_result(data, true);
+
+    println!("Day 7 Part 1: {}", part1_result);
+    println!("Day 7 Part 2: {}", part2_result);
 }
