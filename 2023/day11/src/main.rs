b83983c358c655c1d83ff7a16c28defc2d8e0cf9
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-11 22:33:00 +0100

    Cleanup Day 11

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -7,21 +7,51 @@ struct Map {
     empty_rows: Vec<i64>,
 }
 
+impl Map {
+    fn expand_space(&self, factor: i64) -> Map {
+        let new_galaxies = self
+            .galaxies
+            .iter()
+            .map(|g| {
+                let col_count = self.empty_cols.iter().filter(|x| *x < &g.0).count() as i64;
+                let row_count = self.empty_rows.iter().filter(|y| *y < &g.1).count() as i64;
+
+                (
+                    if col_count != 0 {
+                        g.0 + col_count * (factor - 1)
+                    } else {
+                        g.0
+                    },
+                    if row_count != 0 {
+                        g.1 + row_count * (factor - 1)
+                    } else {
+                        g.1
+                    },
+                )
+            })
+            .collect();
+
+        Map {
+            galaxies: new_galaxies,
+            empty_rows: self.empty_rows.clone(),
+            empty_cols: self.empty_cols.clone(),
+        }
+    }
+}
+
 fn main() {
     let data = fs::read_to_string("day11.txt").expect("Can't read input file");
 
     let map = parse_map(&data);
 
-    let part1_result = calculate_paths(2, map.clone());
-    let part2_result = calculate_paths(1_000_000, map.clone());
+    let part1_result = calculate_paths(map.expand_space(2));
+    let part2_result = calculate_paths(map.expand_space(1_000_000));
 
     println!("Day 11 Part 1: {}", part1_result);
     println!("Day 11 Part 2: {}", part2_result);
 }
 
-fn calculate_paths(factor: i64, mut map: Map) -> i64 {
-    expand_space(&mut map, factor);
-
+fn calculate_paths(mut map: Map) -> i64 {
     let mut paths_lengths: Vec<i64> = Vec::new();
 
     while let Some(galaxy) = &map.galaxies.pop() {
@@ -55,40 +85,12 @@ fn shortest_path(g1: &(i64, i64), g2: &(i64, i64)) -> i64 {
     (g2.0 - g1.0).abs() + (g2.1 - g1.1).abs()
 }
 
-fn expand_space(map: &mut Map, factor: i64) {
-    for index in 0..map.galaxies.len() {
-        let col_count = map
-            .empty_cols
-            .iter()
-            .filter(|x| *x < &map.galaxies[index].0)
-            .count() as i64;
-
-        let row_count = map
-            .empty_rows
-            .iter()
-            .filter(|y| *y < &map.galaxies[index].1)
-            .count() as i64;
-
-        if col_count != 0 {
-            map.galaxies[index].0 += col_count * (factor - 1);
-        }
-
-        if row_count != 0 {
-            map.galaxies[index].1 += row_count * (factor - 1);
-        }
-    }
-}
-
 fn find_empty_space(map: &[Vec<char>]) -> (Vec<i64>, Vec<i64>) {
     let width = map[0].len();
     let mut empty_cols: Vec<i64> = Vec::new();
 
     for x in 0..width {
-        let mut is_empty = true;
-        for row in map.iter() {
-            is_empty &= row[x] == '.'
-        }
-        if is_empty {
+        if map.iter().map(|row| row[x]).all(|c| c == '.') {
             empty_cols.push(x as i64);
         }
     }
@@ -96,7 +98,7 @@ fn find_empty_space(map: &[Vec<char>]) -> (Vec<i64>, Vec<i64>) {
     let mut empty_rows: Vec<i64> = Vec::new();
 
     for (y, row) in map.iter().enumerate() {
-        if row.iter().all(|c| *c == '.') {
+        if row.iter().all(|&c| c == '.') {
             empty_rows.push(y as i64);
         }
     }
