1ba43e0d77723f497d172a725986cc31e3637cc6
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2023-12-14 20:40:15 +0100

    Implements 2023 Day 14 Part 2

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,10 +1,20 @@
+use std::collections::hash_map::DefaultHasher;
 use std::fs;
+use std::hash::{Hash, Hasher};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Hash)]
 enum Tile {
     RoundRock,
     RegularRock,
     Empty,
+}
+
+#[derive(Debug)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
 }
 
 impl Tile {
@@ -26,20 +36,64 @@ fn parse_input(data: &str) -> Map {
         .collect()
 }
 
-fn shift_rocks(map: &mut Map) {
-    let mut moved_rock = true;
+fn shift_rocks(map: &mut Map, direction: Direction) {
+    while shift_rocks_step(map, &direction) {}
+}
 
-    while moved_rock {
-        moved_rock = false;
-        for y in 1..map.len() {
-            for x in 0..map[y].len() {
-                if map[y][x] == Tile::RoundRock && map[y - 1][x] == Tile::Empty {
-                    map[y - 1][x] = Tile::RoundRock;
-                    map[y][x] = Tile::Empty;
-                    moved_rock = true;
+fn shift_rocks_step(map: &mut Map, direction: &Direction) -> bool {
+    let mut moved_rock = false;
+
+    match direction {
+        Direction::North => {
+            for y in 1..map.len() {
+                for x in 0..map[y].len() {
+                    moved_rock |= move_rock(map, (x, y), direction);
                 }
             }
         }
+        Direction::East => {
+            for y in 0..map.len() {
+                for x in (0..map[y].len() - 1).rev() {
+                    moved_rock |= move_rock(map, (x, y), direction);
+                }
+            }
+        }
+        Direction::South => {
+            for y in (0..map.len() - 1).rev() {
+                for x in 0..map[y].len() {
+                    moved_rock |= move_rock(map, (x, y), direction);
+                }
+            }
+        }
+        Direction::West => {
+            for y in 0..map.len() {
+                for x in 1..map[y].len() {
+                    moved_rock |= move_rock(map, (x, y), direction);
+                }
+            }
+        }
+    }
+
+    moved_rock
+}
+
+fn move_rock(map: &mut Map, point: (usize, usize), direction: &Direction) -> bool {
+    let target_point = match direction {
+        Direction::North => (point.0, point.1 - 1),
+        Direction::East => (point.0 + 1, point.1),
+        Direction::South => (point.0, point.1 + 1),
+        Direction::West => (point.0 - 1, point.1),
+    };
+
+    if map[point.1][point.0] == Tile::RoundRock
+        && map[target_point.1][target_point.0] == Tile::Empty
+    {
+        map[target_point.1][target_point.0] = Tile::RoundRock;
+        map[point.1][point.0] = Tile::Empty;
+
+        true
+    } else {
+        false
     }
 }
 
@@ -60,12 +114,45 @@ fn calculate_load(map: &Map) -> usize {
         .sum()
 }
 
+fn run_spin_cycle(map: &mut Map) {
+    for direction in [
+        Direction::North,
+        Direction::West,
+        Direction::South,
+        Direction::East,
+    ] {
+        shift_rocks(map, direction);
+    }
+}
+
+fn calculate_hash(map: &Map) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    map.hash(&mut hasher);
+    hasher.finish()
+}
+
 fn main() {
     let data = fs::read_to_string("day14.txt").expect("Can't read input file");
 
     let mut map = parse_input(&data);
-    shift_rocks(&mut map);
+    shift_rocks(&mut map, Direction::North);
     let part1_result = calculate_load(&map);
-
     println!("Day 14 Part 1: {}", part1_result);
+
+    let mut map = parse_input(&data);
+    let mut cache: Vec<(u64, usize)> = vec![(calculate_hash(&map), calculate_load(&map))];
+
+    for n in 0..1_000_000_000 {
+        run_spin_cycle(&mut map);
+        let hash = calculate_hash(&map);
+        let load = calculate_load(&map);
+
+        if let Some(start) = cache.iter().position(|&item| item == (hash, load)) {
+            let rest = (1_000_000_000 - start) % (n - start + 1);
+            println!("Day 14 Part 2: {}", cache[rest + start].1);
+            break;
+        }
+
+        cache.push((hash, load));
+    }
 }
