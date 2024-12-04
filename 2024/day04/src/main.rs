4246de3c90266dba449c44382d444418fb5b5dbd
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2024-12-04 09:54:10 +0100

    Implements 2024 Day 4 Part 1

---
+++ 2024/day04/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let data = include_str!("../day04.txt");
+
+    let width = data.find('\n').unwrap() + 1;
+    let mut part1_count = 0;
+
+    for index in 0..data.len() {
+        match &data[index..index + 1] {
+            "X" => {
+                if match_horizontal(data, index, "XMAS") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width, "XMAS") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width + 1, "XMAS") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width - 1, "XMAS") {
+                    part1_count += 1;
+                }
+            }
+            "S" => {
+                if match_horizontal(data, index, "SAMX") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width, "SAMX") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width + 1, "SAMX") {
+                    part1_count += 1;
+                }
+                if match_vertical(data, index, width - 1, "SAMX") {
+                    part1_count += 1;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("Day 04 Part 1: {}", part1_count);
+    // println!("Day 04 Part 2: {}", part2_count);
+}
+
+fn match_horizontal(input: &str, index: usize, word: &str) -> bool {
+    if word.len() + index > input.len() {
+        return false;
+    }
+
+    input[index..index + word.len()].eq(word)
+}
+
+fn match_vertical(input: &str, index: usize, width: usize, word: &str) -> bool {
+    let mut start = index;
+
+    for char in word.chars() {
+        if start + 1 > input.len() {
+            return false;
+        }
+
+        if input[start..start + 1] != char.to_string() {
+            return false;
+        }
+
+        start += width;
+    }
+
+    true
+}
