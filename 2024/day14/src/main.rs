e13ecb32fcdc06cafff6ce7213b026339b54b126
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2024-12-14 13:50:59 +0100

    Implements 2024 Day 14 Part 2

--- 2024/day14/src/main.rs
+++ 2024/day14/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -14,6 +15,7 @@ impl Point {
     }
 }
 
+#[derive(Clone)]
 struct Robot {
     pos: Point,
     vel: Point,
@@ -38,7 +40,7 @@ impl Robot {
         }
     }
 
-    fn sumulate(&mut self, map_width: i32, map_height: i32, ticks: i32) {
+    fn simulate(&mut self, map_width: i32, map_height: i32, ticks: i32) {
         self.pos.x += self.vel.x * ticks;
         self.pos.y += self.vel.y * ticks;
 
@@ -53,34 +55,113 @@ impl Robot {
             self.pos.y += map_height;
         }
     }
+
+    fn period(&self, map_width: i32, map_height: i32) -> i32 {
+        let x_period = map_width / gcd(self.vel.x.abs(), map_width);
+        let y_period = map_height / gcd(self.pos.y, map_height);
+
+        lcm(x_period, y_period)
+    }
 }
 
 fn main() {
-    let ticks = 100;
     let map_width = 101;
     let map_height = 103;
     let data = include_str!("../day14.txt");
 
-    let mut robots = data.lines().map(Robot::from_str).collect::<Vec<_>>();
+    let robots = data.lines().map(Robot::from_str).collect::<Vec<_>>();
 
+    let max_ticks = robots
+        .iter()
+        .map(|robot| robot.period(map_width, map_height))
+        .reduce(lcm)
+        .expect("No robots found");
+
+    let part1_result = part1(robots.clone(), map_width, map_height, 100);
+    let part2_result = part2(robots, map_width, map_height, max_ticks);
+
+    println!("Day 14 Part 1: {:?}", part1_result);
+    println!("Day 14 Part 1: {:?}", part2_result);
+}
+
+fn part1(mut robots: Vec<Robot>, map_width: i32, map_height: i32, ticks: i32) -> i32 {
     for robot in robots.iter_mut() {
-        robot.sumulate(map_width, map_height, ticks);
+        robot.simulate(map_width, map_height, ticks);
     }
 
     let map_center_x = (map_width - 1) / 2;
     let map_center_y = (map_height - 1) / 2;
 
-    let quadrants = robots
-        .iter()
-        .fold((0, 0, 0, 0), |(tl, tr, bl, br), robot| match robot.pos {
-            Point { x, y } if x < map_center_x && y < map_center_y => (tl + 1, tr, bl, br),
-            Point { x, y } if x > map_center_x && y < map_center_y => (tl, tr + 1, bl, br),
-            Point { x, y } if x < map_center_x && y > map_center_y => (tl, tr, bl + 1, br),
-            Point { x, y } if x > map_center_x && y > map_center_y => (tl, tr, bl, br + 1),
-            _ => (tl, tr, bl, br),
-        });
+    let mut tl = 0;
+    let mut tr = 0;
+    let mut bl = 0;
+    let mut br = 0;
 
-    let part1_result = quadrants.0 * quadrants.1 * quadrants.2 * quadrants.3;
+    for robot in robots.iter() {
+        match robot.pos {
+            Point { x, y } if x < map_center_x && y < map_center_y => tl += 1,
+            Point { x, y } if x > map_center_x && y < map_center_y => tr += 1,
+            Point { x, y } if x < map_center_x && y > map_center_y => bl += 1,
+            Point { x, y } if x > map_center_x && y > map_center_y => br += 1,
+            _ => {}
+        }
+    }
 
-    println!("Day 14 Part 1: {:?}", part1_result);
+    tl * tr * bl * br
+}
+
+fn part2(mut robots: Vec<Robot>, map_width: i32, map_height: i32, ticks: i32) -> i32 {
+    let mut max_streak = 0;
+    let mut tick_of_max_streak = 0;
+
+    let mut map = vec![vec![false; map_width as usize]; map_height as usize];
+
+    for tick in 1..ticks {
+        for robot in robots.iter_mut() {
+            robot.simulate(map_width, map_height, 1);
+            map[robot.pos.y as usize][robot.pos.x as usize] = true;
+        }
+
+        let longest_streak = map
+            .iter()
+            .map(|row| {
+                row.iter().fold((0, 0), |(current, longest), x| {
+                    if *x {
+                        (current + 1, longest.max(current + 1))
+                    } else {
+                        (0, longest)
+                    }
+                })
+            })
+            .map(|(_, longest)| longest)
+            .max()
+            .unwrap();
+
+        if longest_streak > max_streak {
+            max_streak = longest_streak;
+            tick_of_max_streak = tick;
+        }
+
+        for row in map.iter_mut() {
+            row.fill(false);
+        }
+    }
+
+    tick_of_max_streak
+}
+
+fn gcd(a: i32, b: i32) -> i32 {
+    let (mut a, mut b) = if a < b { (b, a) } else { (a, b) };
+
+    while b != 0 {
+        let tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+
+    a
+}
+
+fn lcm(a: i32, b: i32) -> i32 {
+    (a * b).abs() / gcd(a, b)
 }
