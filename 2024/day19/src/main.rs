fd48ce1e1017603e3b5060d57fc33dfc43cd89a5
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2024-12-19 20:47:23 +0100

    Implements 2024 Day 19 Part 2

--- 2024/day19/src/main.rs
+++ 2024/day19/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let data = include_str!("../day19.txt");
 
@@ -12,6 +14,16 @@ fn main() {
         .count();
 
     println!("Day 19 Part 1: {}", part1_result);
+
+    let mut cache: HashMap<&str, usize> = HashMap::new();
+
+    let part2_result = designs
+        .iter()
+        .filter(|d| is_design_valid(d, &towels))
+        .map(|d| count_possible_arrangements(d, &towels, &mut cache))
+        .sum::<usize>();
+
+    println!("Day 19 Part 2: {}", part2_result);
 }
 
 fn is_design_valid(design: &str, towels: &[&str]) -> bool {
@@ -28,4 +40,30 @@ fn is_design_valid(design: &str, towels: &[&str]) -> bool {
     }
 
     false
+}
+
+fn count_possible_arrangements<'a>(
+    design: &'a str,
+    towels: &[&str],
+    cache: &mut HashMap<&'a str, usize>,
+) -> usize {
+    if design.is_empty() {
+        return 1;
+    }
+
+    let mut count = 0;
+
+    for towel in towels {
+        if let Some(sub_design) = design.strip_prefix(towel) {
+            if let Some(c) = cache.get(sub_design) {
+                count += c;
+            } else {
+                let c = count_possible_arrangements(sub_design, towels, cache);
+                cache.insert(sub_design, c);
+                count += c;
+            }
+        }
+    }
+
+    count
 }
