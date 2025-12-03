5f625b78c42084b567964a5e2a783c8edd900200
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2025-12-03 18:01:37 +0100

    Implements 2025 Day 03 Part 2

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -21,15 +21,35 @@ fn main() {
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect();
 
-    let mut sum = 0;
+    let mut sum = 0_u64;
 
     for bank in &banks {
         assert!(bank.len() >= 2);
         let (first, index) = find_largest(&bank[..bank.len() - 1]);
         let (second, _) = find_largest(&bank[index + 1..]);
 
-        sum += first.to_digit(10).unwrap() * 10 + second.to_digit(10).unwrap();
+        sum += first.to_digit(10).unwrap() as u64 * 10 + second.to_digit(10).unwrap() as u64;
     }
 
     println!("Day 03 Part 1: {}", sum);
+
+    sum = 0;
+
+    let mut bats: Vec<char> = Vec::new();
+    let mut current_index;
+
+    for bank in &banks {
+        bats.clear();
+        current_index = 0;
+
+        for offset in (0..12).rev() {
+            let (bat, index) = find_largest(&bank[current_index..bank.len() - offset]);
+            bats.push(bat);
+            current_index += index + 1;
+        }
+
+        sum += bats.iter().collect::<String>().parse::<u64>().unwrap();
+    }
+
+    println!("Day 03 Part 2: {}", sum);
 }
