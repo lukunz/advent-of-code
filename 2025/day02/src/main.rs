97520e98850452fd3ab1f8bf497a7f075430e988
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2025-12-03 01:59:12 +0100

    Implements 2025 Day 02 Part 2

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,11 +1,52 @@
-fn split_to_number(data: &str, split: usize) -> (u64, u64) {
-    let (hi, lo) = data.split_at(split);
+use std::collections::HashSet;
 
-    (hi.parse::<u64>().unwrap(), lo.parse::<u64>().unwrap())
+fn calculate_id(n: u64, num_repeats: usize) -> u64 {
+    let mag = 10_u64.pow(n.checked_ilog10().unwrap_or(0) + 1);
+
+    let mut result: u64 = n;
+    for _ in 1..num_repeats {
+        result = result * mag + n;
+    }
+
+    result
 }
 
-fn calculate_id(n: u64) -> u64 {
-    10_u64.pow(n.checked_ilog10().unwrap_or(0) + 1) * n + n
+fn normalize_ranges(ranges: Vec<(&str, &str)>) -> Vec<(String, String)> {
+    let mut normalized_ranges: Vec<(String, String)> = Vec::new();
+
+    for (start, end) in ranges {
+        assert!(end.len() - start.len() < 2);
+
+        if end.len() - start.len() == 0 {
+            normalized_ranges.push((start.to_string(), end.to_string()));
+        } else {
+            let split_point = 10_u64.pow(end.len() as u32 - 1);
+
+            normalized_ranges.push((split_point.to_string(), end.to_string()));
+            normalized_ranges.push((start.to_string(), (split_point - 1).to_string()));
+        }
+    }
+
+    normalized_ranges
+}
+
+fn find_ids(start: &str, end: &str, num_repeats: usize, ids: &mut HashSet<u64>) {
+    if !start.len().is_multiple_of(num_repeats) {
+        return;
+    }
+
+    let pattern_len = start.len() / num_repeats;
+    let range = (start.parse::<u64>().unwrap())..=(end.parse::<u64>().unwrap());
+    let start_part = start[..pattern_len].parse::<u64>().unwrap();
+    let end_part = end[..pattern_len].parse::<u64>().unwrap();
+
+    for n in start_part..=end_part {
+        let id = calculate_id(n, num_repeats);
+
+        if range.contains(&id) {
+            ids.insert(id);
+        }
+    }
 }
 
 fn main() {
@@ -16,56 +57,21 @@ fn main() {
         .map(|range| range.split_once('-').unwrap())
         .collect::<Vec<(&str, &str)>>();
 
-    let mut sum: u64 = 0;
+    let ranges = normalize_ranges(ranges);
 
-    let mut valid_ranges: Vec<(String, String)> = Vec::new();
+    let mut result: HashSet<u64> = HashSet::new();
 
-    for (start, end) in ranges {
-        assert!(end.len() - start.len() < 2);
-        let start_is_even = start.len() % 2 == 0;
+    for (start, end) in &ranges {
+        find_ids(start, end, 2, &mut result);
+    }
 
-        if start.len() % 2 != 0 && end.len() % 2 != 0 {
-            continue;
-        }
+    println!("Result Day 2 Part 1: {}", result.iter().sum::<u64>());
 
-        if end.len() - start.len() == 0 {
-            valid_ranges.push((start.to_string(), end.to_string()));
-            continue;
-        }
-
-        if !start_is_even {
-            let new_start = 10_u64.pow(end.len() as u32 - 1).to_string();
-            valid_ranges.push((new_start, end.to_string()));
-        } else {
-            let new_end = (10_u64.pow(end.len() as u32 - 1) - 1).to_string();
-            valid_ranges.push((start.to_string(), new_end));
+    for (start, end) in &ranges {
+        for num_repeats in 3..=start.len() {
+            find_ids(start, end, num_repeats, &mut result);
         }
     }
 
-    for (start, end) in valid_ranges {
-        let split = start.len() / 2;
-
-        let (start_hi, start_lo) = split_to_number(&start, split);
-        let (end_hi, end_lo) = split_to_number(&end, split);
-
-        let diff = end_hi - start_hi;
-
-        if diff > 0 {
-            for i in (start_hi + 1)..end_hi {
-                sum += calculate_id(i);
-            }
-
-            if start_hi >= start_lo {
-                sum += calculate_id(start_hi);
-            }
-
-            if end_hi <= end_lo {
-                sum += calculate_id(end_hi);
-            }
-        } else if start_hi >= start_lo && start_hi <= end_lo {
-            sum += calculate_id(start_hi);
-        }
-    }
-
-    println!("Result Day 2 Part 1: {}", sum);
+    println!("Result Day 2 Part 2: {}", result.iter().sum::<u64>());
 }
