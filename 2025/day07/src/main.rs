de47b15e0e75f3ab71532d8c073594575023310d
Lukas Kunz <1230442+lukunz@users.noreply.github.com>
2025-12-07 13:42:32 +0100

    Implements 2025 Day 07 Part 2

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(PartialEq, Clone, Debug)]
 enum Tile {
@@ -19,11 +19,25 @@ impl Tile {
 struct Map {
     tiles: Vec<Vec<Tile>>,
     start: Point,
+    width: usize,
+    height: usize,
 }
 
 impl Map {
+    fn new(tiles: Vec<Vec<Tile>>, start: Point) -> Self {
+        let width = tiles.len();
+        let height = tiles[0].len();
+
+        Self {
+            tiles,
+            start,
+            width,
+            height,
+        }
+    }
+
     fn contains(&self, point: &Point) -> bool {
-        self.tiles.len() > point.x && self.tiles[0].len() > point.y
+        self.width > point.x && self.height > point.y
     }
 }
 
@@ -34,33 +48,23 @@ struct Point {
 }
 
 fn parse_input(input: &str) -> Map {
-    let data: Vec<Vec<char>> = input
+    let tiles: Vec<Vec<Tile>> = input
         .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
+        .map(|line| line.chars().map(Tile::from_char).collect::<Vec<Tile>>())
         .collect();
-
-    let mut tiles: Vec<Vec<Tile>> = Vec::with_capacity(data[0].len());
-    for _ in 0..data[0].len() {
-        tiles.push(Vec::new());
-    }
 
     let mut start: Option<Point> = None;
 
-    for (y, row) in data.iter().enumerate() {
-        for (x, &c) in row.iter().enumerate() {
+    'outer: for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
             if c == 'S' {
                 start = Some(Point { x, y });
-                tiles[x].push(Tile::Empty);
-            } else {
-                tiles[x].push(Tile::from_char(c));
+                break 'outer;
             }
         }
     }
 
-    Map {
-        tiles,
-        start: start.unwrap(),
-    }
+    Map::new(tiles, start.unwrap())
 }
 
 fn part1(map: &Map) -> u64 {
@@ -81,7 +85,7 @@ fn part1(map: &Map) -> u64 {
 
         visited.insert(point.clone());
 
-        match map.tiles[point.x][point.y] {
+        match map.tiles[point.y][point.x] {
             Tile::Empty => {
                 next.push_back(Point {
                     x: point.x,
@@ -107,12 +111,63 @@ fn part1(map: &Map) -> u64 {
     split_count
 }
 
+fn collect_splits(map: &Map) -> Vec<Point> {
+    let mut splits = Vec::new();
+
+    for (y, row) in map.tiles.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            if tile == &Tile::Splitter {
+                splits.push(Point { x, y });
+            }
+        }
+    }
+
+    splits
+}
+
+fn count_splits(map: &Map, counted_splits: &HashMap<Point, u64>, split: &Point) -> u64 {
+    let mut split_count = 0;
+
+    'outer: for x in [split.x - 1, split.x + 1] {
+        for y in split.y..map.height {
+            let point = Point { x, y };
+            if let Some(sub_count) = counted_splits.get(&point) {
+                split_count += sub_count;
+                continue 'outer;
+            }
+        }
+        split_count += 1;
+    }
+
+    split_count
+}
+
+fn part2(map: &Map) -> u64 {
+    let mut splits = collect_splits(map);
+    let mut counted_splits: HashMap<Point, u64> = HashMap::new();
+
+    while let Some(split) = splits.pop() {
+        let count = count_splits(map, &counted_splits, &split);
+        counted_splits.insert(split, count);
+    }
+
+    for y in map.start.y..map.height {
+        if map.tiles[y][map.start.x] == Tile::Splitter {
+            return *counted_splits.get(&Point { x: map.start.x, y }).unwrap();
+        }
+    }
+
+    1
+}
+
 fn main() {
     let input = include_str!("../day07.txt");
 
     let map = parse_input(input);
 
     let part1_result = part1(&map);
-
     println!("Day 07 Part 1: {}", part1_result);
+
+    let part2_result = part2(&map);
+    println!("Day 07 Part 2: {}", part2_result);
 }
